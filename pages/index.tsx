687973767fa0072cd1a0b19fcf792135c09de084
seili <ilies365@gmail.com>
2020-05-10 12:20:46 +0200

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <h2>To Do List</h2>
+      </header>
+      <main>Ilies</main>
+    </>
+  );
+};
+export default App;
